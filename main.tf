a82a776cc6ccf5213635ab37ec9a31cdea8ef85d
AKHILKUMARBALIVADA <akhilkumar21345@gmail.com>
2024-07-29 09:22:34 +0530

    added vpc in main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resource "aws_instance" "apple-1" {
-  ami           = "ami-068e0f1a600cd311c"
-  instance_type = "t2.micro"
-  key_name      = "sivapem-1"
+# resource "aws_instance" "apple-1" {
+#   ami           = "ami-068e0f1a600cd311c"
+#   instance_type = "t2.micro"
+#   key_name      = "sivapem-1"
+#   tags = {
+#     Name = "apple-1"
+#   }
+# }
+resource "aws_vpc" "apple-12" {
+  cidr_block = "192.0.0.0/16"
   tags = {
-    Name = "apple-1"
+    Name = "apple-12"
   }
 }
